39efe4df6a14cfd19d1ceae0e46a40aaba001487
sineline <guillem.gimenez@gmail.com>
2022-05-01 15:41:48 +0200

    sample file updated

--- scraper.py
+++ scraper.py
@@ -1,9 +1,5 @@
 import parse_classes as pc
 
 
-pr = pc.inforesidencias()
-# resis = pr.get_residencies()
-
-singleRes = pr.get_residence_data({'name':'testres', 
-                                   'url':'https://www.inforesidencias.com/centros/residencia/2322/centres-geriatrics-malgrat'})
-print(singleRes)
+pr = pc.inforesidencias(provincia='Lleida', output='residencias')
+pr.get_residencies() 
